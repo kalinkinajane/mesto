2a6321c50369ac20be0434c5b38baf5585c5e7c3
kalinkinajane <62453022+kalinkinajane@users.noreply.github.com>
2020-05-14 22:28:43 +1100

    внесены изменения в popup__input,добавлены атрибуты и ком в js

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,14 @@ const page = document.querySelector('.page');
 const popup = page.querySelector('.popup');
 const buttonEdit = page.querySelector('.profile__button-edit');
 const popupClose = page.querySelector('.popup__close');
-let inputName = page.querySelector('.popup__input-container_type_name');
-let inputJob = page.querySelector('.popup__input-container_type_job');
+let inputName = page.querySelector('.popup__item_type_name');
+let inputJob = page.querySelector('.popup__item_type_job');
 let profileName = page.querySelector('.profile__name');
 let profileJob = page.querySelector('.profile__job');
 const addButton = page.querySelector('.popup__button');
 let formElement = page.querySelector('.popup__container');
+
+/*функция открытия и закрытия блока popup*/
 
 function toggleForm() {
     popup.classList.toggle('popup_open');
@@ -17,6 +19,7 @@ function toggleForm() {
         inputJob.value = profileJob.textContent;
     }
 }
+/* Обработчик отправки формы */
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -24,7 +27,7 @@ function formSubmitHandler(evt) {
     profileJob.textContent = inputJob.value;
     toggleForm()
 }
-
+/*Слушатели собитий */
 formElement.addEventListener('submit', formSubmitHandler);
 buttonEdit.addEventListener('click', toggleForm);
 popupClose.addEventListener('click', toggleForm);
