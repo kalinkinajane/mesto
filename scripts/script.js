ea3c27d15b623ee893132ee2d10479bbf8fa0536
kalinkinajane <62453022+kalinkinajane@users.noreply.github.com>
2020-05-29 16:48:23 +1100

    Проэктая работа 5: добавление изображений с помощью js, форма добавления карточки, лайки карточек,удаление

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,10 @@ let inputJob = page.querySelector('.popup__item_type_job');
 let profileName = page.querySelector('.profile__name');
 let profileJob = page.querySelector('.profile__job');
 const addButton = page.querySelector('.popup__button');
-let formElement = page.querySelector('.popup__container');
+const formElement = page.querySelector('.popup__container');
+const formAddElement = page.querySelector('.popup-add__container');
+
+
 
 /*функция открытия и закрытия блока popup*/
 
@@ -31,3 +34,107 @@ function formSubmitHandler(evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 buttonEdit.addEventListener('click', toggleForm);
 popupClose.addEventListener('click', toggleForm);
+
+
+/* Массив карточек*/
+const initialCards = [
+    {
+        name: 'Приморский край',
+        link: '../images/primorsk-krai.jpg'
+    },
+    {
+        name: 'Выборг',
+        link: '../images/monrepo-vyborg.jpg'
+    },
+    {
+        name: 'Карелия',
+        link: '../images/karelia.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: '../images/kamchatka.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: '../images/baikal.jpg'
+    },
+    {
+        name: 'Красная площадь',
+        link: '../images/red-square.jpg'
+    },
+];
+const placesSection = document.querySelector('.places');
+const plaseTemplate = document.querySelector('.place-element').content;
+const popupImage = document.querySelector('.popup-view__image');
+const popupCaption = document.querySelector('.popup-view__caption');
+
+/* Функция создания карточек plece*/
+function addPlace(item) {
+    const placeElement = plaseTemplate.cloneNode(true);
+    placeElement.querySelector('.place__image').src = item.link;
+    placeElement.querySelector('.place__title').textContent = item.name;
+    placeElement.querySelector('.place__like').addEventListener('click', function (evt) {
+        /*Лайк карточек */
+        evt.target.classList.toggle('place__like_active');
+    });
+    /*Удаление карточек */
+    const deleteButton = placeElement.querySelector('.place__remove');
+    deleteButton.addEventListener('click', function () {
+        const placeItem = deleteButton.closest('.place');
+        placeItem.remove();
+    });
+    /* Щткрытие popup-view с картинкой */
+    placeElement.querySelector('.place__image').addEventListener('click', function (evt) { 
+        evt.target.classList.toggle('popup-view__open');
+        popupImage.src = item.link;
+        popupCaption.textContent = item.name;
+        togglePopupView();
+    });
+    placesSection.prepend(placeElement);
+};
+/* перебор массива*/
+function render() {
+    initialCards.forEach(addPlace);
+}
+render();
+
+/*функция открытия и закрытия блока popup_add*/
+const buttonAdd = page.querySelector('.profile__button-add');
+const popupAdd = page.querySelector('.popup-add');
+const closePopup = page.querySelector('.popup-add__close');
+function togglePopup() {
+    popupAdd.classList.toggle('popup-add__open');
+    if (popupAdd.classList.contains('popup-add__open')) {
+    }
+}
+buttonAdd.addEventListener('click', togglePopup);
+closePopup.addEventListener('click', togglePopup);
+
+const inputPlace = page.querySelector('.popup__item_type_place-name');
+const inputlink = page.querySelector('.popup__item_type_link');
+/*Добавление карточки */
+formAddElement.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    /*Добавление элемента в массив */
+    let addInitialCards = {
+        name: '',
+        link: ''
+    }
+    addInitialCards.name = inputPlace.value;
+    addInitialCards.link = inputlink.value;
+    initialCards.unshift(addInitialCards);
+    addPlace(addInitialCards);
+    togglePopup();
+});
+
+/*функция открытия и закрытия блока popup-view__image*/
+const popupView = document.querySelector('.popup-view');
+const cloceView = document.querySelector('.popup-view__close');
+function togglePopupView() {
+    popupView.classList.toggle('popup-view__open');
+    if ( popupView.classList.contains('popup-view__open')) {
+    }
+};
+cloceView.addEventListener('click', togglePopupView);
+
+
